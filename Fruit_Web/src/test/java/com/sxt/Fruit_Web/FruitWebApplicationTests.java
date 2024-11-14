879309f9a2791e1399fe5d2c05d47022c35e67c3
Winnie-star846 <13823585034@163.com>
2024-11-14 13:30:02 +0800

    Add files via upload

---
+++ Fruit_Web/src/test/java/com/sxt/Fruit_Web/FruitWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxt.Fruit_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
