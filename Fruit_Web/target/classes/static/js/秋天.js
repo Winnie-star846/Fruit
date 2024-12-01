d2898ac8aab7a032885efd7e97f1adeedbabbba7
Winnie-star846 <13823585034@163.com>
2024-12-01 17:31:08 +0800

    Delete Fruit_Web directory

--- Fruit_Web/target/classes/static/js/秋天.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const fruits = document.querySelectorAll('.fruit');
- 
-    fruits.forEach(fruit => {
-        fruit.addEventListener('click', () => {
-            alert(`Name: ${fruit.getAttribute('data-name')}\nSeason: ${fruit.getAttribute('data-season')}\nType: ${fruit.getAttribute('data-type')}\n\n${fruit.querySelector('p').textContent}`);
-        });
-    });
-});
